a878e637716209625c704e91fa2fe22d33b8701a
Jing <19191124+JING1201@users.noreply.github.com>
2021-05-18 14:12:12 -0400

    Separate frame code to a function

--- piano-keys/piano_keys.js
+++ piano-keys/piano_keys.js
@@ -48,6 +48,26 @@ const BlackKey = function (note, positionX) {
     }
 }
 
+const buildFrame = function(leftPositionX, rightPositionX) {
+    const frameLeft = BABYLON.MeshBuilder.CreateBox("frameLeft", {width: 2.4/scale, height: (keyHeight+2)/scale, depth: 15/scale}, scene);
+    frameLeft.position = new BABYLON.Vector3(leftPositionX/scale, (keyHeight+2)/2/scale, 4/scale);
+    const frameRight = BABYLON.MeshBuilder.CreateBox("frameRight", {width: 2.4/scale, height: (keyHeight+2)/scale, depth: 15/scale}, scene);
+    frameRight.position = new BABYLON.Vector3(rightPositionX/scale, (keyHeight+2)/2/scale, 4/scale);
+    const frameBack = BABYLON.MeshBuilder.CreateBox("frameBack", {width: (2.4*52)/scale, height: (keyHeight+10)/scale, depth: 5/scale}, scene);
+    frameBack.position = new BABYLON.Vector3(2.4*3.5/scale, (keyHeight+10)/2/scale, 9/scale);
+    const wingLeft = BABYLON.MeshBuilder.CreateBox("wingLeft", {width: 2.4/scale, height: 8/scale, depth: 5/scale}, scene);
+    wingLeft.position =  new BABYLON.Vector3(leftPositionX/scale, (keyHeight+6)/scale, 9/scale);
+    const wingRight = BABYLON.MeshBuilder.CreateBox("wingRight", {width: 2.4/scale, height: 8/scale, depth: 5/scale}, scene);
+    wingRight.position =  new BABYLON.Vector3(rightPositionX/scale, (keyHeight+6)/scale, 9/scale);
+    
+    const frame = BABYLON.Mesh.MergeMeshes([frameLeft, frameRight, frameBack, wingLeft, wingRight], true, false, null, false, false);
+    const frameMat = new BABYLON.StandardMaterial("frameMat");
+    frameMat.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    frame.material = frameMat;
+
+    return frame;
+}
+
 
 
 const createScene = async function () {
@@ -103,20 +123,7 @@ const createScene = async function () {
     keys.add(WhiteKey("C", 2.3, 2.3, 0, -2.4*6).build(scene, 8, referencePositionX))
 
     // Piano Frame
-    const frameLeft = BABYLON.MeshBuilder.CreateBox("frameLeft", {width: 2.4/scale, height: (keyHeight+2)/scale, depth: 15/scale}, scene);
-    frameLeft.position = new BABYLON.Vector3(-2.4*23/scale, (keyHeight+2)/2/scale, 4/scale);
-    const frameRight = BABYLON.MeshBuilder.CreateBox("frameRight", {width: 2.4/scale, height: (keyHeight+2)/scale, depth: 15/scale}, scene);
-    frameRight.position = new BABYLON.Vector3((referencePositionX-2.4*5)/scale, (keyHeight+2)/2/scale, 4/scale);
-    const frameBack = BABYLON.MeshBuilder.CreateBox("frameBack", {width: (2.4*52)/scale, height: (keyHeight+10)/scale, depth: 5/scale}, scene);
-    frameBack.position = new BABYLON.Vector3(2.4*3.5/scale, (keyHeight+10)/2/scale, 9/scale);
-    const wingLeft = BABYLON.MeshBuilder.CreateBox("wingLeft", {width: 2.4/scale, height: 8/scale, depth: 5/scale}, scene);
-    wingLeft.position =  new BABYLON.Vector3(-2.4*23/scale, (keyHeight+6)/scale, 9/scale);
-    const wingRight = BABYLON.MeshBuilder.CreateBox("wingRight", {width: 2.4/scale, height: 8/scale, depth: 5/scale}, scene);
-    wingRight.position =  new BABYLON.Vector3((referencePositionX-2.4*5)/scale, (keyHeight+6)/scale, 9/scale);
-    const frame = BABYLON.Mesh.MergeMeshes([frameLeft, frameRight, frameBack, wingLeft, wingRight], true, false, null, false, false);
-    const frameMat = new BABYLON.StandardMaterial("frameMat");
-    frameMat.diffuseColor = new BABYLON.Color3(0, 0, 0);
-    frame.material = frameMat;
+    const frame = buildFrame(-2.4*23, referencePositionX-2.4*5)
 
     const pointerToKey = new Map()
     const piano = await Soundfont.instrument(new AudioContext(), 'acoustic_grand_piano');
@@ -156,7 +163,7 @@ const createScene = async function () {
     const xrHelper = await scene.createDefaultXRExperienceAsync();
     
     const featuresManager = xrHelper.baseExperience.featuresManager;
-    
+
     const pointerSelection = featuresManager.enableFeature(BABYLON.WebXRFeatureName.POINTER_SELECTION, "stable" /* or latest */, {
         xrInput: xrHelper.input,
         enablePointerSelectionOnAllControllers: true        
